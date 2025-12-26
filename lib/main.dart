62dae7177fb08dd329ac10e2206614a79cdfdc72
Hamouda <mohamed.hamouda7897@gmail.com>
2025-12-26 11:58:30 +0200

    UI almost done for onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: OnboardingScreen.routeName,
       routes: {
         OnboardingScreen.routeName: (context) => OnboardingScreen(),
--- lib/screens/onboarding_screen/onboarding_screen.dart
+++ lib/screens/onboarding_screen/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class OnboardingScreen extends StatelessWidget {
   static const String routeName = "OnbaordingScreen";
@@ -8,7 +9,109 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Image.asset("assets/images/logo.png")),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Image.asset("assets/images/logo.png"),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          spacing: 28,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("assets/images/creative.png", width: double.infinity),
+
+            Text(
+              "Personalize Your Experience",
+              style: GoogleFonts.inter(
+                color: Color(0xFF5669FF),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            Text(
+              "Choose your preferred theme and language to get started with a comfortable, tailored experience that suits your style.",
+              style: GoogleFonts.inter(
+                color: Color(0xFF1C1C1C),
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+
+            Column(
+              spacing: 16,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Language",
+                      style: GoogleFonts.inter(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF5669FF),
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          Image.asset("assets/images/us.png"),
+                          Image.asset("assets/images/eg.png"),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Theme",
+                      style: GoogleFonts.inter(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF5669FF),
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        children: [
+                          Image.asset("assets/images/sun.png"),
+                          Image.asset("assets/images/moon.png"),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  backgroundColor: Color(0xFF5669FF),
+                ),
+                child: Text(
+                  "Let's Start",
+                  style: GoogleFonts.inter(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
