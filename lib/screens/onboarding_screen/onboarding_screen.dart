b04cc5531fae157ff264807fee578f7f1ce1624c
Hamouda <mohamed.hamouda7897@gmail.com>
2025-12-26 11:48:06 +0200

    assets added

--- lib/screens/onboarding_screen/onboarding_screen.dart
+++ lib/screens/onboarding_screen/onboarding_screen.dart
@@ -7,6 +7,8 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Image.asset("assets/images/logo.png")),
+    );
   }
 }
