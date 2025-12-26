e64211b5fcc2bbbe6572989e74359bfd682af4a1
Hamouda <mohamed.hamouda7897@gmail.com>
2025-12-26 11:44:04 +0200

    init

---
+++ lib/screens/onboarding_screen/onboarding_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  static const String routeName = "OnbaordingScreen";
+
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
